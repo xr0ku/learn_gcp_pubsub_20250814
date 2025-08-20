d11d4cd8c71e4637c68473830c99f4e3da4ce1b2
xr0ku <moonlightbtb@gmail.com>
2025-08-20 20:31:37 +0900

    chore: add frontend.yaml

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES ? "/learn_gcp_pubsub_20250814" : "/",
   server: {
     port: 5173,
     proxy: {
